d944df4d59fce63667e01c1ec15f503a0ed36219
Pavel Savastseika <p.savastseika@arammeem.com>
2024-12-30 21:10:57 +0400

    finish Chapter 2

--- src/02 BuiltIn Functions/utilfuncs_start.py
+++ src/02 BuiltIn Functions/utilfuncs_start.py
@@ -4,15 +4,19 @@
 def main():
     # use any() and all() to test sequences for boolean values
     list1 = [1, 2, 3, 0, 5, 6]
+    print(f"List: {list1}")
     
-    # TODO: any will return true if any of the sequence values are true
-    
-    # TODO: all will return true only if all values are true
-    
-    # TODO: min and max will return minimum and maximum values in a sequence
-    
-    # TODO: Use sum() to sum up all of the values in a sequence
-    
+    # any will return true if any of the sequence values are true
+    print(f"Any: {any(list1)}")
+    # all will return true only if all values are true
+    print(f"All: {all(list1)}")
+    # min and max will return minimum and maximum values in a sequence
+    print(f"Min: {min(list1)}")
+    print(f"Max: {max(list1)}")
+    # Use sum() to sum up all of the values in a sequence
+    print(f"Sum: {sum(list1)}")
+
+
 if __name__ == "__main__":
     main()
     
